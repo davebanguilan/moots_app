a4991e7f4e27e1d419560aeb6f36763677708726
Dave Emilson Banguilan <dbanguilan@you-source.com>
2021-08-15 23:23:44 +0800

    lint fix

--- src/app/pages/sign-in/sign-in.page.ts
+++ src/app/pages/sign-in/sign-in.page.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.page.html',
-  styleUrls: ['./sign-in.page.scss'],
+  styleUrls: ['./sign-in.page.scss']
 })
 export class SignInPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
